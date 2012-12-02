ee484516b02c4396e58c578beca8c9eadb5490a7
Vinnie Falco <vinnie.falco@gmail.com>
2012-12-02 10:59:32 -0800

    Update .jucer and project files

--- Build/Jucer/JuceLibraryCode/BinaryData.h
+++ Build/Jucer/JuceLibraryCode/BinaryData.h
@@ -7,19 +7,19 @@
 namespace BinaryData
 {
     extern const char*   CHANGES;
-    const int            CHANGESSize = 853;
+    const int            CHANGESSize = 823;
 
     extern const char*   Doxyfile;
-    const int            DoxyfileSize = 78384;
+    const int            DoxyfileSize = 76585;
 
     extern const char*   Manual_html;
-    const int            Manual_htmlSize = 55204;
+    const int            Manual_htmlSize = 53493;
 
     extern const char*   README_md;
-    const int            README_mdSize = 32950;
+    const int            README_mdSize = 3473;
 
     extern const char*   UnitTests_lua;
-    const int            UnitTests_luaSize = 3877;
+    const int            UnitTests_luaSize = 3770;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
